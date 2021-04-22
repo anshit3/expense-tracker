ee33b2c381c9b5fff45d787918ea22b267e8b464
anshit3 <dranrox@gmail.com>
2021-04-22 15:16:05 +0530

    Add state for each form element

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,31 +1,70 @@
 import './ExpenseForm.css';
+import { useState } from 'react';
 
-const ExpenseForm = () => (
-  <form>
-    <div className="new-expense_controls">
-      <div className="new-expense__control">
-        <label htmlFor="title">
-          Title
-          <input id="title" type="text" />
-        </label>
+const ExpenseForm = () => {
+  const [title, setTitle] = useState('Where did you spend money?');
+  const [amount, setAmount] = useState('0.0');
+  const [date, setDate] = useState('');
+
+  const titleChangeHandler = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const amountChangeHandler = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const dateChangeHandler = (e) => {
+    setDate(e.target.value);
+  };
+  return (
+    <form>
+      <div className="new-expense_controls">
+        <div className="new-expense__control">
+          <label htmlFor="title">
+            Title
+            <input
+              id="title"
+              type="text"
+              required="true"
+              value={title}
+              onChange={titleChangeHandler}
+            />
+          </label>
+        </div>
+        <div className="new-expense__control">
+          <label htmlFor="amount">
+            Amount
+            <input
+              id="amount"
+              type="number"
+              min="0.01"
+              step="0.01"
+              value={amount}
+              onChange={amountChangeHandler}
+            />
+          </label>
+        </div>
+        <div className="new-expense__control">
+          <label htmlFor="date">
+            Date
+            <input
+              id="date"
+              type="date"
+              min="2011-01-01"
+              max="2023-12-31"
+              value={date}
+              required="true"
+              onChange={dateChangeHandler}
+            />
+          </label>
+        </div>
+        <div className="new-expense__actions">
+          <button type="submit">Add Expense</button>
+        </div>
       </div>
-      <div className="new-expense__control">
-        <label htmlFor="amount">
-          Amount
-          <input id="amount" type="number" min="0.01" step="0.01" />
-        </label>
-      </div>
-      <div className="new-expense__control">
-        <label htmlFor="date">
-          Date
-          <input id="date" type="date" min="2011-01-01" max="2023-12-31" />
-        </label>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
-      </div>
-    </div>
-  </form>
-);
+    </form>
+  );
+};
 
 export default ExpenseForm;
